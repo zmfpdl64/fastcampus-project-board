60fa74c19742353eef42fca1230c04390fb48c48
leewoojin <zmfpdl64@naver.com>
2022-07-13 20:23:48 +0900

    #18 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.
    현재 테스트는 통과 하지 못한다.

---
+++ src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -0,0 +1,90 @@
+package com.fastcampus.fastcampusprojectboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("View 컨트롤러 - 게시글")
+@WebMvcTest(ArticleController.class)
+class ArticleControllerTest {
+
+    private final MockMvc mvc;
+    //@Autowired 필수적으로 써줘야 테스트에서는 동작한다.
+
+    ArticleControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현 중")
+    @DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상호출")
+    @Test
+    void givenNothing_whenRequestArticles_thenReturnsArticlesView() throws Exception {
+    //Given
+
+    //When
+        mvc.perform(get("/articles"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attributeExists("articles"));
+
+    //Then
+    }
+    @Disabled("구현 중")
+
+    @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
+    @Test
+    void givenNothing_whenRequestArticle_thenReturnsArticleView() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(get("/articles/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/detail"))
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("articleComments"));
+
+        //Then
+    }
+    @Disabled("구현 중")
+
+    @DisplayName("[view][GET] 게시글 검색 전용 페이지 - 정상호출")
+    @Test
+    void givenNothing_whenRequestArticleSearchView_thenReturnsArticlesSearchView() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(get("/articles/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search"));
+
+
+        //Then
+    }
+    @Disabled("구현 중")
+
+    @DisplayName("[view][GET] 해시태그 검색 페이지 - 정상호출")
+    @Test
+    void givenNothing_whenRequestArticleHashtagSearchView_thenReturnsArticlesHashtagSearchView() throws Exception {
+        //Given
+
+        //When
+        mvc.perform(get("/articles/search-hashtag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("articles/search-hashtag"));
+
+
+        //Then
+    }
+}
