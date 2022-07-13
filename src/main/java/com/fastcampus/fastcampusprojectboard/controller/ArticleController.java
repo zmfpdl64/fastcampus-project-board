c814ae9482d4e1a21a98912b339e6e7e99b9f5d2
leewoojin <zmfpdl64@naver.com>
2022-07-13 20:23:48 +0900

    #18 - 게시글 관련 뷰 테스트 작성
    어떤 경로로 뷰를 열어줄지 테스트로 명세를 뽑는다.
    현재 테스트는 통과 하지 못한다.

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.fastcampus.fastcampusprojectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+
+
+
+}
