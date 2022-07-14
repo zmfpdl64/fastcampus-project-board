bfb5945f6e2838895d322272f79e2e51c33d17fb
leewoojin <zmfpdl64@naver.com>
2022-07-14 16:49:38 +0900

    #18 - 단일 포스트 조회 컨트롤러 작성
    단일 포스트를 확인할 수 있는 조건들을 충족하게 작성했다.
    1. 해당 게시물
    2. 해당 게시물의 댓글
    3. Pathvariable 사용
    4. datatil.html 로 렌더링

--- src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.fastcampus.fastcampusprojectboard.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,6 +17,13 @@ public class ArticleController {
     public String articles(ModelMap map) {
         map.addAttribute("articles", List.of());
         return "articles/index";
+    }
+    @GetMapping("/{articleId}")
+    public String article_detail(ModelMap map, @PathVariable(name = "articleId") Long articleId) {
+
+        map.addAttribute("articles", null);
+        map.addAttribute("articleComments", List.of());
+        return "articles/detail";
     }
 
 
