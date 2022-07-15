08bff25e060cc929a169ccdf4a481d452598f0e0
leewoojin <zmfpdl64@naver.com>
2022-07-15 16:39:13 +0900

    #18 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정
    지금은 테스트 단계이고 아직 인증 구현기능 단계
    전이므로  모든 인증을 오픈함
    그리고 폼 로그인 활성화를 해서 로그인 뷰가 그려지게 함
    최신 스프링 2.7 시큐리티가 권장하는 방식을 이용함
     * https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

---
+++ src/main/java/com/fastcampus/fastcampusprojectboard/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.fastcampus.fastcampusprojectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityfilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin().and()
+                .build();
+    }
+
+}
