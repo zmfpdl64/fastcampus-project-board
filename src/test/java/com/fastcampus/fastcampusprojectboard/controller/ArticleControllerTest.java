a806acd6a1bd7abec99cd4752fb6ed9205ae5d7c
leewoojin <zmfpdl64@naver.com>
2022-07-23 23:26:56 +0900

    #31 - 컨트롤러 정렬 기능 추가
    입력오는 페이지의 번호에 따라서 계산해 다시 사용자에게
    정렬된 페이지를 전달한다.
    테스트 케이스도 barNumbers가 포함되어있는지 등 데이터를 추가했다.

--- src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/fastcampusprojectboard/controller/ArticleControllerTest.java
@@ -7,27 +7,26 @@ import com.fastcampus.fastcampusprojectboard.dto.ArticleDto;
 import com.fastcampus.fastcampusprojectboard.dto.ArticleWithCommentsDto;
 import com.fastcampus.fastcampusprojectboard.dto.UserAccountDto;
 import com.fastcampus.fastcampusprojectboard.service.ArticleService;
+import com.fastcampus.fastcampusprojectboard.service.PagenationService;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -42,6 +41,7 @@ class ArticleControllerTest {
     //@Autowired 필수적으로 써줘야 테스트에서는 동작한다.
 
     @MockBean private ArticleService articleService;
+    @MockBean private PagenationService pagenationService;
 
     ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
@@ -52,6 +52,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestArticles_thenReturnsArticlesView() throws Exception {
     //Given
         given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(Page.empty());
+        given(pagenationService.getPaginationBarNumbers(anyInt(),anyInt())).willReturn(List.of(0,1,2,3,4));
 
 
     //When
@@ -59,11 +60,49 @@ class ArticleControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/index"))
-                .andExpect(model().attributeExists("articles"));
+                .andExpect(model().attributeExists("articles"))
+                .andExpect(model().attributeExists("paginationBarNumbers"));
     //Then
         then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
+        then(pagenationService).should().getPaginationBarNumbers(anyInt(),anyInt());
 
     }
+
+    @DisplayName("[view] [GET] 게시글 리스트 (게시판) 페이지 - 페이징, 정렬 기능")
+    @Test
+    public void givenNothing_whenRequestingArticlesView_thenReturnsArticles() throws Exception {
+
+
+        //Given
+        String sortName = "title";
+        String direction = "desc";
+        int pageNumber = 0;
+        int pageSize = 5;
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, Sort.by(Sort.Order.desc(sortName)));
+        List<Integer> barNumbers = List.of(1,2,3,4,5);
+
+        given(articleService.searchArticles(null, null, pageable)).willReturn(Page.empty());
+        given(pagenationService.getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages())).willReturn(barNumbers);
+
+
+        //When'
+        mvc.perform(get("/articles")
+                .queryParam("page", String.valueOf(pageNumber))
+                .queryParam("size", String.valueOf(pageSize))
+                .queryParam("sort", sortName+ "," + direction)
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(view().name("articles/index"))
+                .andExpect(model().attribute("paginationBarNumbers", barNumbers));
+
+        //Then
+        then(articleService).should().searchArticles( null, null,pageable);
+        then(pagenationService).should().getPaginationBarNumbers(pageable.getPageNumber(), Page.empty().getTotalPages());
+
+    }
+
+
     @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상호출")
     @Test
