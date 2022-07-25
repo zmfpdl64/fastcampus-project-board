5e5df4711af2a54af541ff325e23ea8a8c5c95df
leewoojin <zmfpdl64@naver.com>
2022-07-25 22:37:42 +0900

    #28 - SearchType 한글 내용 추가
    타임리프에 한글을 타입을 넘겨줄때 어떤 타입으로 검색할 수 있는지
    한글로 볼 수 있게 descriptionfiled를 추가했다.

--- src/main/java/com/fastcampus/fastcampusprojectboard/domain/type/SearchType.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/domain/type/SearchType.java
@@ -1,5 +1,18 @@
 package com.fastcampus.fastcampusprojectboard.domain.type;
 
+import lombok.Getter;
+
 public enum SearchType {
-    TITLE, CONTENT, HASHTAG, ID, NICKNAME
+    CONTENT("내용"),
+    HASHTAG("해시태그"),
+    ID("유저 ID"),
+    NICKNAME("닉네임"),
+    TITLE("제목");
+
+    @Getter private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
+
 }
