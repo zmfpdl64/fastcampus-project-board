043b2956cb91604f1c31dbc525b1e1a2e5156421
leewoojin <zmfpdl64@naver.com>
2022-07-29 22:56:48 +0900

    #34 - 잘못된 data 변경
    id값은 entity가 자동으로 생성해주는건데 그것을 컬럼에다가 추가했다
    그래서 이 정보를 삭제해 개선하였다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,10 @@
 -- 테스트 계정
 
 -- TODO: 테스트용이지만 비밀번호가 노출된 데이터 세팅, 개선하는 것이 좋을 지 고민해 보자.
-insert into user_account (user_id, user_password, email, nickname, memo, created_at, created_by, modified_at, modified_by) values
-('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno', now(), 'uno', now(), 'uno');
-insert into user_account (user_id, user_password, email, nickname, memo, created_at, created_by, modified_at, modified_by) values
-('wooojin', 'asdf1234', 'wooojin', 'wooojin@mail.com', 'I am wooojin', now(), 'wooojin', now(), 'wooojin');
+insert into user_account ( created_at, created_by, modified_at, modified_by, email, memo, nickname, user_id, user_password) values
+    (now(), 'woojin', now(), 'woojin', 'mail@mail.com', null, 'woojin', 'woojin', 'pwd');
+insert into user_account (created_at, created_by, modified_at, modified_by, email, memo, nickname, user_id, user_password) values
+    (now(), 'woojin2', now(), 'woojin2', 'mail2@mail.com', null, 'woojin2', 'woojin2', 'pwd');
 
 -- 123 게시글
 insert into article (user_account_id, title, content, hashtag, created_by, modified_by, created_at, modified_at) values
