b68cdc464327daed6daaf37a422bc218cb4c97d6
leewoojin <zmfpdl64@naver.com>
2022-08-04 20:22:50 +0900

    #37 - 오류가 발생하는 테스트 설정들 수정
    data sql에서는 문법 오류
    jparepositorytest에서는 pk값이 중복되어나는 오류
    applicationtest는 필요없는 테스트케이스 삭제함

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
-('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno.', now(), 'uno', now(), 'uno')
+insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
+    ('uno', 'asdf1234', 'Uno', 'uno@mail.com', 'I am Uno.', now(), 'uno', now(), 'uno')
 ;
 insert into user_account (user_id, user_password, nickname, email, memo, created_at, created_by, modified_at, modified_by) values
 ('uno2', 'asdf1234', 'Uno2', 'uno2@mail.com', 'I am Uno2.', now(), 'uno2', now(), 'uno2')
