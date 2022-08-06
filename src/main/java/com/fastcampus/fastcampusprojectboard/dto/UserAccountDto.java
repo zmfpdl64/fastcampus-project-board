a77963ffe9b7867c946fce17d508f3113a18740d
leewoojin <zmfpdl64@naver.com>
2022-08-07 00:34:18 +0900

    #40 - 게시글, 유저 dto, request 생성 및 변경
    articlerequest
    -> 사용자에게 입력받은 form request값을 entity로 변환하기 위한  클래스이다.
    articledto, useraccountdto
    -> 이건 새로운 생성자를 선언하여 굳이 null로 채우지 않아도
    자동으로 null로 채울 수 있게 생성자를 추가해주었다.

--- src/main/java/com/fastcampus/fastcampusprojectboard/dto/UserAccountDto.java
+++ src/main/java/com/fastcampus/fastcampusprojectboard/dto/UserAccountDto.java
@@ -19,6 +19,9 @@ public record UserAccountDto(
     public static UserAccountDto of(LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy, String userId, String userPassword, String email, String nickname, String memo) {
         return new UserAccountDto(createdAt, createdBy, modifiedAt, modifiedBy, userId, userPassword, email, nickname, memo);
     }
+    public static UserAccountDto of(String userId, String userPassword, String email, String nickname, String memo) {
+        return new UserAccountDto(null, null, null, null, "uno", "asdf1234", "uno@mail.com", "uno", "memo");
+    }
 
     public static UserAccountDto from(UserAccount entity) {
         return new UserAccountDto(
