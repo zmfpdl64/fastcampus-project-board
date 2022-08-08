473f783f2b8ea6ee79d910bacd4eab21b8a4758e
leewoojin <zmfpdl64@naver.com>
2022-08-09 00:29:48 +0900

    #29 - 테스트 코드에서 필요 없는 부분을 제거함
    필요없는 부분을 제거해 테스트 코드의 오류를 방지했다.

--- src/test/java/com/fastcampus/fastcampusprojectboard/controller/AuthControllerTest.java
+++ src/test/java/com/fastcampus/fastcampusprojectboard/controller/AuthControllerTest.java
@@ -18,7 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @DisplayName("View 컨트롤러 - 게시글")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(MainController.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
